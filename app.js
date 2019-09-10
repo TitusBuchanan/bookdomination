b1e5399569d3ec9096260d030f5c275148934f61
Titus Buchanan Jr <titusbuchananjr@gmail.com>
2019-09-09 20:11:20 -0400

    added a form

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ list.addEventListener('click',function(e){
         list.removeChild(li);
     }
 })
+
+//add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit',function(e){
+epreventDefault();
+const value = addForm.querySelector('input[type="text"]').value;
+console.log(value);
+})
